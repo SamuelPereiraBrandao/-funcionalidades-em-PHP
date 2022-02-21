054e72e77af68bb2614cfc4229d73e3ffd65ddeb
Samuel Pereira Brandão <samuelpbrandao58@gmail.com>
2022-02-20 23:10:59 -0300

    ok
    ok

---
+++ include-e-require/index.php
@@ -0,0 +1,11 @@
+<?php include_once 'header.php'; ?>
+
+<?php 
+echo "Olá Pessoal!";
+?>
+
+
+<?php require_once 'footer.php' 
+/// requiere = aparece um erro, e não exibe nada!
+/// include = aparece um erro, mas continua exibindo!
+?>
